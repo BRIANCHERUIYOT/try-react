3bcd6f870bc72b58e4d69dc3f576ab00b40adfb9
BRIANCHERUIYOT <kipkiruicheruiyot41@gmail.com>
2022-07-17 01:44:57 +0300

    adds nav components

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from   "react";
+
+
+function Navbar() {
+    return (
+      <div className="Navbar">
+        <h1>
+            my navigation bar is here
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Navbar;
