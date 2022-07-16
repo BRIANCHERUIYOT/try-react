09c9287c33f7b1ad9f14e424af7a1a1d1493faf8
BRIANCHERUIYOT <kipkiruicheruiyot41@gmail.com>
2022-07-16 15:44:18 +0300

    my first commit

---
+++ src/Hom.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hom() {
+    return (
+      <div className="App">
+        <h1>
+            This is a Hom
+        </h1>
+      </div>
+    );
+  }
+  
+  export default Hom;
